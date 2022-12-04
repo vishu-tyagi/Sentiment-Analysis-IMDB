39e0b039cfb01e8b93d8ac1c18e8ca0078d08786
vishu-tyagi <vishweshwartyagiv@gmail.com>
2022-12-04 01:22:18 +0000

    add environment

---
+++ src/sentiment-analysis/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="sentiment-analysis",
+    version="0.0.1",
+    description="Package for sentiment analysis",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9"
+)
